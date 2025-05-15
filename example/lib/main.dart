ae717ab332a5effd5826cb2bb178d13f73d11b35
IzacPS <izaccknd@gmail.com>
2025-05-14 23:27:47 -0300

    Refactor.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,105 +1,3 @@
-// import 'dart:math';
-//
-// import 'package:flutter/material.dart';
-//
-// void main() => runApp(MyApp());
-//
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//         title: 'Flutter 3D transition',
-//         theme: ThemeData(
-//           primarySwatch: Colors.green,
-//         ),
-//         home: Page1());
-//   }
-// }
-//
-// class Page1 extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: Colors.blue[50],
-//       body: Center(
-//         child: ElevatedButton(
-//           onPressed: () => Navigator.of(context).pushReplacement(
-//             Pseudo3dRouteBuilder(this, Page2()),
-//           ),
-//           child: Text('change the page'),
-//         ),
-//       ),
-//     );
-//   }
-// }
-//
-// class Page2 extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: Colors.pink[50],
-//       body: Center(
-//         child: ElevatedButton(
-//           onPressed: () => Navigator.of(context).pushReplacement(
-//             Pseudo3dRouteBuilder(this, Page1()),
-//           ),
-//           child: Text('change the page'),
-//         ),
-//       ),
-//     );
-//   }
-// }
-//
-// class Pseudo3dRouteBuilder extends PageRouteBuilder {
-//   final Widget enterPage;
-//   final Widget exitPage;
-//   Pseudo3dRouteBuilder(this.exitPage, this.enterPage)
-//       : super(
-//           pageBuilder: (context, animation, secondaryAnimation) => enterPage,
-//           transitionsBuilder: _transitionsBuilder(exitPage, enterPage),
-//         );
-//
-//   static _transitionsBuilder(exitPage, enterPage) =>
-//       (context, animation, secondaryAnimation, child) {
-//         return Stack(
-//           children: <Widget>[
-//             SlideTransition(
-//               position: Tween<Offset>(
-//                 begin: Offset.zero,
-//                 end: Offset(-1.0, 0.0),
-//               ).animate(animation),
-//               child: Container(
-//                 color: Colors.white,
-//                 child: Transform(
-//                   transform: Matrix4.identity()
-//                     ..setEntry(3, 2, 0.003)
-//                     ..rotateY(pi / 2 * animation.value),
-//                   alignment: FractionalOffset.centerRight,
-//                   child: exitPage,
-//                 ),
-//               ),
-//             ),
-//             SlideTransition(
-//               position: Tween<Offset>(
-//                 begin: Offset(1.0, 0.0),
-//                 end: Offset.zero,
-//               ).animate(animation),
-//               child: Container(
-//                 color: Colors.white,
-//                 child: Transform(
-//                   transform: Matrix4.identity()
-//                     ..setEntry(3, 2, 0.003)
-//                     ..rotateY(pi / 2 * (animation.value - 1)),
-//                   alignment: FractionalOffset.centerLeft,
-//                   child: enterPage,
-//                 ),
-//               ),
-//             )
-//           ],
-//         );
-//       };
-// }
-
 import 'package:flutter/material.dart';
 import 'package:updates/updates.dart';
 
@@ -156,6 +54,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         useMaterial3: true,
@@ -188,90 +87,39 @@ class _TestUpdatesState extends State<TestUpdates> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Updates<Data, String>(
-          controller: _controller,
-          statusColorBuilder: (context, item) {
-            return Colors.orange;
-          },
-          avatarBuilder: (context, item) {
-            return Image.asset(item?.url ?? "");
-          },
-          descriptionBuilder: (context, item) {
-            return Text(item?.name ?? "");
-          },
-          backgroundBuilder: (context, item) {
-            return Container(
-                color: Colors.grey[200], child: Image.asset(item ?? ""));
-          },
-          foregroundBuilder: (context, item) {
-            return Align(
-                alignment: Alignment.bottomLeft, child: Text(item.toString()));
-          },
-          generateContentData: (item) {
-            return item?.content ?? [];
-          },
+        child: Center(
+          child: Updates<Data, (String, String)>(
+            controller: _controller,
+            statusColorBuilder: (context, item) {
+              return Colors.orange;
+            },
+            avatarBuilder: (context, item) {
+              return Image.asset(item?.url ?? "");
+            },
+            descriptionBuilder: (context, item) {
+              return Text(item?.name ?? "");
+            },
+            backgroundBuilder: (context, item) {
+              return Container(
+                  color: Colors.grey[200], child: Image.asset(item?.$2 ?? ""));
+            },
+            foregroundBuilder: (context, item) {
+              return Align(
+                  alignment: Alignment.topLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      item?.$1 ?? "",
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                  ));
+            },
+            generateContentData: (item) {
+              return item?.content.map((e) => (item.name, e)).toList() ?? [];
+            },
+          ),
         ),
       ),
     );
   }
 }
-
-// // class _TestUpdatesState extends State<TestUpdates> {
-// //   late final ContentController _controller;
-// //
-// //   @override
-// //   void initState() {
-// //     super.initState();
-// //     _controller = ContentController(numItems: 3);
-// //     _controller.addOnMaxIndexCallback(() {
-// //       debugPrint("-------------- reached max");
-// //     });
-// //     _controller.addOnMinIndexCallback(() {
-// //       debugPrint("-------------- reached min");
-// //     });
-// //   }
-// //
-// //   @override
-// //   Widget build(BuildContext context) {
-// //     return Scaffold(
-// //       appBar: AppBar(backgroundColor: Colors.black),
-// //       body: SafeArea(
-// //         child: Content<String>(
-// //           contentController: _controller,
-// //           items: ["data 1", "data 2", "data 3"],
-// //           backgroundBuilder: (context, content) {
-// //             return Center(
-// //               child: Text(content!),
-// //             );
-// //           },
-// //           foregroundBuilder: (context, content, index) {
-// //             return Align(
-// //                 alignment: Alignment.topCenter,
-// //                 child: Text("content: $content"));
-// //           },
-// //         ),
-// //       ),
-// //       bottomNavigationBar: SizedBox(
-// //         height: 54,
-// //         child: Padding(
-// //           padding: const EdgeInsets.all(8.0),
-// //           child: Row(
-// //             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-// //             children: [
-// //               ElevatedButton.icon(
-// //                   onPressed: () {
-// //                     _controller.previous();
-// //                   },
-// //                   label: Icon(Icons.keyboard_arrow_left)),
-// //               ElevatedButton.icon(
-// //                   onPressed: () {
-// //                     _controller.next();
-// //                   },
-// //                   label: Icon(Icons.keyboard_arrow_right)),
-// //             ],
-// //           ),
-// //         ),
-// //       ),
-// //     );
-// //   }
-// // }
